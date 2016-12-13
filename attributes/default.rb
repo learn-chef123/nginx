90cbc7bbc858c7481d42ce70d45e46ec891859d6
okram999 <nirish.okram@gmail.com>
2016-12-13 16:21:36 -0600

    changes to the default recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['nginx']['basic_auth'] = false
 default['nginx']['port'] = 80
-override['nginx']['version'] = '1.10.2-1.el6'
+override['nginx']['version'] = '1.10.2-1.el7'
 default['nginx']['name'] = '_'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,30 +4,32 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-include_recipe 'yum'
-
-package 'nss' do
-  action :upgrade
+packs = %w(epel-release vim).each do |p|
+  package p do
+  not_if "rpm -qa | grep -i #{p}"
+ end
 end
-
-include_recipe 'yum-epel'
+#
+# execute 'yum install epel-release -y'
+#
+# execute 'yum install vim -y'
 
 package 'nginx' do
   version "#{node[:nginx][:version]}"
   not_if "rpm -qa | grep -i nginx-#{node[:nginx][:version]}"
 end
 
-
-template "/etc/nginx/conf.d/default.conf" do
-  source "nginx_default.conf.erb"
-  mode 00644
-  variables(
-      :listen_port => node[:nginx][:port],
-      :server_name => node[:nginx][:name],
-      :basic_auth_enabled => node[:nginx][:basic_auth],
-      )
-  notifies :restart, "service[nginx]", :delayed
-end
+#
+# template "/etc/nginx/conf.d/default.conf" do
+#   source "nginx_default.conf.erb"
+#   mode 00644
+#   variables(
+#       :listen_port => node[:nginx][:port],
+#       :server_name => node[:nginx][:name],
+#       :basic_auth_enabled => node[:nginx][:basic_auth],
+#       )
+#   notifies :restart, "service[nginx]", :delayed
+# end
 
 service "nginx" do
   action [:enable, :start]
