0c4f18da7ce839d085f795f1622fbc688369b7e3
okram999 <nirish.okram@gmail.com>
2016-12-16 14:13:19 -0600

    adds templates and test for the recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@ default['nginx']['basic_auth'] = false
 default['nginx']['port'] = 80
 override['nginx']['version'] = '1.10.2-1.el7'
 default['nginx']['name'] = '_'
+default['nginx']['proxy_pass'] = 'http://localhost:8081'
--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -5,14 +5,28 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
+# unless os.windows?
+#   describe user('root') do
+#     it { should exist }
+#     skip 'This is an example test, replace with your own test.'
+#   end
+# end
+#
+# describe port(80) do
+#   it { should_not be_listening }
+#   skip 'This is an example test, replace with your own test.'
+# end
+
+describe package('epel-release') do
+  it { should be_installed }
 end
 
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe package('vim-enhanced') do
+  it { should be_installed }
+end
+
+describe service('nginx') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
 end
